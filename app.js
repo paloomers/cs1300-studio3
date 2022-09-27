521109e4a8991e6c648f78a1c6b83c59a3827c4d
Paloma Salseda <palomasalseda@palomas-mbp.devices.brown.edu>
2022-09-27 17:52:28 -0400

    done~

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+   //console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -26,14 +26,23 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+   console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
-
+  const filtered = fruitsArray.filter(fruitsArray => fruitsArray.id < 15);
+  console.log(filtered); 
+  
   // TODO: Create a new HTML element to display your data
+  const newEl = document.createElement('pre');
 
+  let word = ""; 
+  for (const element of filtered){
+    word += JSON.stringify(element, undefined, 2); 
+  }
+  newEl.innerHTML = word;
   // TODO: Append your new element to the page
+  gallery.append(newEl); 
 
 }
 
